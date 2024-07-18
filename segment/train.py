dad21038950633031750ea940cdf6af9b68f7e1e
the0807 <the0807.eom@gmail.com>
2024-07-18 22:41:41 +0900

    segment 1. train

---
+++ segment/train.py
@@ -0,0 +1,35 @@
+import os
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.data.datasets import register_coco_instances
+
+train_path = 'dataset/Preprocessed_2DSS/training/coco'
+val_path = 'dataset/Preprocessed_2DSS/validation/coco'
+test_path = 'dataset/Preprocessed_2DSS/test/coco'
+
+train_json_path = os.path.join(train_path, 'training_coco.json')
+val_json_path = os.path.join(val_path, 'validation_coco.json')
+test_json_path = os.path.join(test_path, 'test_coco.json')
+
+register_coco_instances("2DSS_train", {}, train_json_path, train_path)
+register_coco_instances("2DSS_val", {}, val_json_path, val_path)
+register_coco_instances("2DSS_test", {}, test_json_path, test_path)
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("2DSS_train")
+cfg.DATASETS.TEST = ("2DSS_val")
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 4  # This is the real "batch size" commonly known to deep learning people
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 24
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=False)
+trainer.train()
