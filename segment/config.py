11f8ed0839be297abe6fdfae31ea289628c14cd3
the0807 <the0807.eom@gmail.com>
2024-07-28 23:14:43 +0900

    change config 1. Mask R-CNN on Cityscapes dataset

--- segment/config.py
+++ segment/config.py
@@ -5,20 +5,18 @@ from detectron2.config import get_cfg
 from detectron2 import model_zoo
 
 cfg = get_cfg()
-#cfg.merge_from_file(model_zoo.get_config_file("Cityscapes/mask_rcnn_R_50_FPN.yaml"))
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.merge_from_file(model_zoo.get_config_file("Cityscapes/mask_rcnn_R_50_FPN.yaml"))
 cfg.DATASETS.TRAIN = ("2DSS_train",)
 cfg.DATASETS.TEST = ("2DSS_val",)
 cfg.MODEL.DEVICE = "cuda"
 cfg.DATALOADER.NUM_WORKERS = 6
 cfg.SOLVER.IMS_PER_BATCH = 6
 cfg.SOLVER.BASE_LR = 0.001
-cfg.SOLVER.MAX_ITER = 350000
+cfg.SOLVER.MAX_ITER = 25000
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 24
 cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 24
-#cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("Cityscapes/mask_rcnn_R_50_FPN.yaml")
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("Cityscapes/mask_rcnn_R_50_FPN.yaml")
 
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
